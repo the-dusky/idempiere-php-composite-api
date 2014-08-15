4d02ae0f418de70d0ef5d92d5e53a1203cac36a5
Sandy Corsillo <sandy@maxcorsillo.com>
2014-08-15 09:19:01 -0400

    readme: example json - id_api: javascript camel caseing

--- README.md
+++ README.md
@@ -2,3 +2,45 @@ php_idempiere_composite_api
 ===========================
 
 A php wrapper for the composite api in Idempiere
+
+request_params can be sent in as json or an associative php array.
+
+example json:
+
+{
+    "settings" : {
+        "urlEndpoint" : "",
+        "user" : "**Username**",
+        "password" : "**Password**",
+        "language" : "en_US",
+        "clientId" : "11",
+        "roleId" : "50004",
+        "orgId" : "11",
+        "warehouseId" : "103",
+        "stage" : "9"
+    },
+    "call" : [
+        {
+            "type" : "createUpdateData",
+            "preCommit" : "false",
+            "postCommit" : "false",
+            "serviceName" : "createBPartner",
+            "table" : "c_bpartner",
+            "keyValPairs" : {
+                "X" : "Y"
+            }
+        },
+
+        {
+            "type" : "setDocAction",
+            "preCommit" : "",
+            "postCommit" : "",
+            "serviceName" : "",
+            "table" : "",
+            "keyValPairs" : {
+                "idColumn" : "Y",
+                "action" : "CO"
+            }
+        }
+    ]
+}
--- src/id_api.php
+++ src/id_api.php
@@ -2,6 +2,7 @@
 
     class IdApi {
 
+        private $request_array;
         private $request;
         private $response;
         private $request_params;
@@ -21,7 +22,7 @@
 
         public function request() {
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL,               $this->request['url']);
+            curl_setopt($ch, CURLOPT_URL,               $this->request_params['settings']['urlEndpoint']);
             curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,    10);
             curl_setopt($ch, CURLOPT_TIMEOUT,           10);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER,    true );
@@ -33,41 +34,52 @@
         }
 
         public function build_request($request_params) {
+
+            if(!is_array($request_params)) {
+                $request_params = json_decode($request_params, true);
+            }
+
             $this->request_params = $request_params;
-            $this->build_request_header();
+
+            $this->set_request_params_settings_defaults();
+
+            $this->build_request_head();
 
             $this->build_request_body();
 
             $this->build_request_footer();
         }
 
-        public function build_request_header() {
-            $header_params = $this->request_params['header'];
+        public function set_request_params_settings_defaults() {
+            if(isset())
+        }
+
+        public function build_request_head() {
             $this->request .= '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:_0="http://idempiere.org/ADInterface/1_0"><soapenv:Header/>';
                 $this->request .= '<soapenv:Body>';
                     $this->request .= '<_0:compositeOperation>';
                         $this->request .= '<_0:CompositeRequest>';
                             $this->request .= '<_0:ADLoginRequest>';
-                                    $this->request .= '<_0:user>WebService</_0:user>';
-                                    $this->request .= '<_0:pass>2Four6Eight10</_0:pass>';
-                                    $this->request .= '<_0:lang>en_US</_0:lang>';
-                                    $this->request .= '<_0:ClientID>11</_0:ClientID>';
-                                    $this->request .= '<_0:RoleID>50004</_0:RoleID>';
-                                    $this->request .= '<_0:OrgID>11</_0:OrgID>';
-                                    $this->request .= '<_0:WarehouseID>103</_0:WarehouseID>';
-                                    $this->request .= '<_0:stage>9</_0:stage>';
+                                    $this->request .= '<_0:user>' .  $this->request_params['settings']['user'] . '</_0:user>';
+                                    $this->request .= '<_0:pass>' .  $this->request_params['settings']['password'] . '</_0:pass>';
+                                    $this->request .= '<_0:lang>' .  $this->request_params['settings']['language'] . '</_0:lang>';
+                                    $this->request .= '<_0:ClientID>' .  $this->request_params['settings']['client'] . '</_0:ClientID>';
+                                    $this->request .= '<_0:RoleID>' .  $this->request_params['settings']['roleId'] . '</_0:RoleID>';
+                                    $this->request .= '<_0:OrgID>' .  $this->request_params['settings']['orgId'] . '</_0:OrgID>';
+                                    $this->request .= '<_0:WarehouseID>' .  $this->request_params['settings']['warehouseId'] . '</_0:WarehouseID>';
+                                    $this->request .= '<_0:stage>' .  $this->request_params['settings']['stage'] . '</_0:stage>';
                             $this->request .= '</_0:ADLoginRequest>';
                             $this->request .= '<_0:serviceType>SyncOrder</_0:serviceType>';
                             $this->request .= '<_0:operations>';
         }
 
         public function build_request_body() {
-            foreach($this->request_params['body'] as $request) {
+            foreach($this->request_params['call'] as $request) {
                 if($request['type'] == 'createUpdateData'){
-                    $this->request .= '<_0:operation preCommit="' . $request['pre_commit'] . '" postCommit="' . $request['post_commit'] . '">';
+                    $this->request .= '<_0:operation preCommit="' . $request['preCommit'] . '" postCommit="' . $request['posCommit'] . '">';
                         $this->request .= '<_0:TargetPort>createUpdateData</_0:TargetPort>';
                         $this->request .= '<_0:ModelCRUD>';
-                            $this->request .= '<_0:serviceType>' . $request['service_name'] . '</_0:serviceType>';
+                            $this->request .= '<_0:serviceType>' . $request['serviceName'] . '</_0:serviceType>';
                             $this->request .= '<_0:TableName>' . $request['table'] . '</_0:TableName>';
                             $this->request .= '<_0:RecordID>0</_0:RecordID>';
                             $this->request .= '<_0:Action>CreateUpdate</_0:Action>';
@@ -85,7 +97,7 @@
                         $this->request .= '</_0:ModelCRUD>';
                     $this->request .= '</_0:operation>';
                 } elseif($request['type'] == 'setDocAction') {
-                    $this->request .= '<_0:operation preCommit="' . $request['pre_commit'] . '" postCommit="' . $request['post_commit'] . '">';
+                    $this->request .= '<_0:operation preCommit="' . $request['preCommit'] . '" postCommit="' . $request['postCommit'] . '">';
                         $this->request .= '<_0:TargetPort>setDocAction</_0:TargetPort>';
                         $this->request .= '<_0:ModelSetDocAction>';
                             $this->request .= '<_0:serviceType>' . $request['service_name'] . '</_0:serviceType>';
